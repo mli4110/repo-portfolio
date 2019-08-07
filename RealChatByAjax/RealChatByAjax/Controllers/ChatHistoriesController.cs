1a718503c39ec59dcb9792578dad9e983df8f364
Meng <lim@lim.com>
2019-08-07 02:05:15 -0400

    Upload portfolio of RealChat

---
+++ RealChatByAjax/RealChatByAjax/Controllers/ChatHistoriesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RealChatByAjax.Models;
+
+namespace RealChatByAjax.Controllers
+{
+    public class ChatHistoriesController : Controller
+    {
+        private ChatMessageDBEntities db = new ChatMessageDBEntities();
+
+        // GET: ChatHistories/Select
+        public ActionResult Select()
+        {
+            return View();
+        }
+
+        // POST: ChatHistories/Select
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Select(string sender)
+        {
+            Session["senderIdentity"] = sender;
+            return RedirectToAction("Create", "ChatHistories");
+        }
+
+        // -- AJAX to get chat messages --
+        public ActionResult GetMessage(string receiver)
+        {
+            string senderIdentity = Session["senderIdentity"] as string;
+
+            List<ChatHistory> messages = new List<ChatHistory>();
+
+            messages = db.ChatHistories.Where(x => (x.Sender == senderIdentity && x.Receiver == receiver) || (x.Receiver == senderIdentity && x.Sender == receiver)).ToList();
+
+            return Json(messages.OrderByDescending(x => x.DateTime), JsonRequestBehavior.AllowGet);
+        }
+
+        // -- AJAX to create chat message --
+        [HttpPost]
+        public ActionResult CreateMessage(ChatHistory chatHistory)
+        {
+            string message;
+            if (chatHistory.Content == null)
+            {
+                message = "ERROR";
+                return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+            }
+            else
+            {
+                chatHistory.DateTime = DateTime.Now;
+
+                db.ChatHistories.Add(chatHistory);
+                db.SaveChanges();
+                message = "SUCCESS";
+                return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+            }
+        }
+
+        // GET: ChatHistories/Create
+        public ActionResult Create()
+        {
+            string senderIdentity = Session["senderIdentity"] as string;
+
+            // -- alternative --
+            //var messages = from m in db.ChatHistories where (m.FromName == fromName) || (m.ToName == fromName) select m;
+            //return View(messages.ToList());
+
+            var messages = db.ChatHistories.Where(x => x.Sender == senderIdentity || x.Receiver == senderIdentity).ToList();
+            return View(messages);
+        }
+
+        // GET: ChatHistories
+        public ActionResult Search(string searchString, string receiver)
+        {
+            string senderIdentity = Session["senderIdentity"] as string;
+
+            List<ChatHistory> messages = new List<ChatHistory>();
+
+            //messages = db.ChatHistories.Where(x => (x.Sender == senderIdentity || x.Receiver == senderIdentity) && x.Content.Contains(searchString)).ToList();
+
+            if (receiver == "")
+            {
+                messages = db.ChatHistories.Where(x => (x.Sender == senderIdentity || x.Receiver == senderIdentity) && x.Content.Contains(searchString)).ToList();
+            }
+            else
+            {
+                messages = db.ChatHistories.Where(x => ((x.Sender == senderIdentity && x.Receiver == receiver) || (x.Receiver == senderIdentity && x.Sender == receiver)) && x.Content.Contains(searchString)).ToList();
+            }
+
+            return View("Search", messages);
+        }
+
+        // GET: ChatHistories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChatHistory chatHistory = db.ChatHistories.Find(id);
+            if (chatHistory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chatHistory);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RealChatByAjax/RealChatByAjax/Scripts/myJS.js
@@ -0,0 +1,134 @@
+﻿
+// hide sender name in dropdown list
+var selectedName = document.getElementById("sender").textContent;
+var selectedOption = "option".concat(selectedName);
+document.getElementById(selectedOption).style.display = "none";
+// ----
+
+var receiverName = "";
+
+//if (receiverName != "") {
+//    setInterval(function () {
+//        LoadData();
+//    }, 5000);
+//}
+
+setInterval(function () {
+    LoadData();
+}, 5000);
+
+
+function LoadData() {
+
+    if (receiverName == "") {
+        document.getElementById("chatMessage").disabled = true;
+        document.getElementById("sendButton").disabled = true;
+        document.getElementById("messageList").hidden = true;
+        document.getElementById("chatReminder").hidden = false;
+    }
+    else {
+        document.getElementById("chatMessage").disabled = false;
+        document.getElementById("sendButton").disabled = false;
+        document.getElementById("messageList").hidden = false;
+        document.getElementById("chatReminder").hidden = true;
+    }
+    $("#messageList tbody tr").remove();
+    $.ajax({
+        type: 'POST',
+        url: '@Url.Action("GetMessage")',
+        dataType: 'json',
+        data: { receiver: receiverName },
+        success: function (data) {
+            //var items = '';
+            $.each(data, function (index, item) {
+                var rows;
+                if (item.FromName == selectedName) {
+                    rows = "<tr>" + "<td class='prtoducttd'>" + "<div class='sentMessage'>" + item.Content + "<br/>" + "Posted on: " + FormatDateString(item.DateTime) + "</div>" + "</td>" + "</tr>";
+                }
+                else {
+                    rows = "<tr>" + "<td class='prtoducttd'>" + "<div class='receivedMessage'>" + item.Content + "<br/>" + "Posted on: " + FormatDateString(item.DateTime) + "</div>" + "</td>" + "</tr>";
+                }
+                $('#messageList tbody').append(rows);
+            });
+        },
+        error: function (ex) {
+            var r = jQuery.parseJSON(response.responseText);
+            alert("Message: " + r.Message);
+            alert("StackTrace: " + r.StackTrace);
+            alert("ExceptionType: " + r.ExceptionType);
+        }
+    });
+    return false;
+}
+
+// filter the chat messages by selected receiver name
+document.getElementById("receiverList").addEventListener("change", function () {
+    receiverName = document.getElementById("receiverList").value;
+    document.getElementById("receiver").innerHTML = receiverName;
+    LoadData();
+    console.log($("#sender").text());
+    console.log($("#receiver").text());
+});
+
+// count the number of character in textarea
+document.getElementById("chatMessage").addEventListener("keyup", function () {
+    var messageLength = $("#chatMessage").val().length;
+    if (messageLength < 99) {
+        document.getElementById("counterMessage").innerHTML = "This message can cantain " + (100 - messageLength) + " more characters.";
+        document.getElementById("counterMessage").style.color = "gray";
+    }
+    else if (messageLength == 99) {
+        document.getElementById("counterMessage").innerHTML = "This message can cantain one more character.";
+        document.getElementById("counterMessage").style.color = "blue";
+    }
+    else {
+        document.getElementById("counterMessage").innerHTML = "Maximum 100 characters per message!";
+        document.getElementById("counterMessage").style.color = "red";
+    }
+});
+
+
+$(document).ready(function () {
+    LoadData();
+    $("#sendButton").click(function () {
+        //alert("");
+        var chat = {};
+        chat.FromName = $("#sender").text();
+        chat.ToName = $("#receiver").text();
+        chat.Content = $("#chatMessage").val();
+        $.ajax({
+            type: "POST",
+            url: '@Url.Action("CreateMessage")',
+            data: '{chatHistory: ' + JSON.stringify(chat) + '}',
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function () {
+                // alert("Data has been added successfully.");
+                LoadData();
+                $("#chatMessage").val("");
+            },
+            error: function () {
+                alert("Error while inserting data. Please note the message can not be empty!");
+            }
+        });
+
+        return false;
+    });
+
+});
+
+
+// convert Datetime format to string for showing in table
+function FormatDateString(logDate) {
+    var d = new Date(parseInt(logDate.substr(6)));
+    var year = d.getFullYear();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+    var hour = d.getHours();
+    var minutes = d.getMinutes();
+    var sec = d.getSeconds();
+
+    return month + "/" + day + "/" + year + " " + hour + ":" + minutes + ":" + sec;
+}
+// ----
+
